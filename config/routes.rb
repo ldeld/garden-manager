02ccbd78faf5a76590270d7fa80099bc2a10046a
Lorenzo Del Castillo Detoeuf <l.delcastillo.97@gmail.com>
2022-02-17 17:55:07 +0100

    finished livecode

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
     resources :plants, only: [:create]
   end
 
-  resources :plants, only: [:destroy]
+  resources :plants, only: [:destroy] do
+    resources :plant_tags, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # get "/plants/:plant_id/plant_tags/new"
+  # post "/plants/:plant_id/plant_tags"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # db/seeds.rb
 Garden.destroy_all if Rails.env.development?
 
-Garden.create!(
+little = Garden.create!(
   name: "My Little Garden",
   banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_1.jpg"
 )
@@ -11,4 +11,26 @@ Garden.create!(
   banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_2.jpg"
 )
 
+Plant.create!(
+  name: "Monstera",
+  image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/monstera.jpg",
+  garden: little
+)
 
+Plant.create!(
+  name: "Philo",
+  image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/philo.jpg",
+  garden: little
+)
+
+Plant.create!(
+  name: "Dieff",
+  image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/dieffenbachia.jpg",
+  garden: little
+)
+
+
+tags = ['Outdoor', 'Indoor', 'Tree', 'Flower', 'Fern', 'Cactus']
+tags.each do |tag|
+  Tag.create!(name: tag)
+end
